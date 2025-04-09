4ba113c0bcbeb21bdde9cb4ef891d68f98adaaf2
Lasya <lasyaedunuri@gmail.com>
2025-04-09 15:09:26 -0400

    Create model.py

---
+++ model.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+
+class GestureClassifier(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(GestureClassifier, self).__init__()
+        self.fc1 = nn.Linear(input_size, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, num_classes)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
